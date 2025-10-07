da0199e134696d891bb96cede8b02fd1cd202d7e
Adamd3vs <theesanovmirjalol12@gmail.com>
2025-10-08 06:36:04 +0900

    feat: yml file

--- app.js
+++ app.js
@@ -175,10 +175,3 @@ document.querySelectorAll('.details-btn').forEach(btn=>{
   });
 });
 
-(() => {
-  const key = 'portfolio_theme';
-  const saved = localStorage.getItem(key);
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const theme = saved || 'light'; // ← make "light" default
-  document.documentElement.setAttribute('data-theme', theme);
-})();
